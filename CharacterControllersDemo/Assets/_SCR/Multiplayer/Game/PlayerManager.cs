ba43414441230bc1d25c6adc360bb221896bf292
Oliver <Olstar_123@users.noreply.github.com>
2021-10-29 15:10:58 -0600

    Solid

--- CharacterControllersDemo/Assets/_SCR/Multiplayer/Game/PlayerManager.cs
+++ CharacterControllersDemo/Assets/_SCR/Multiplayer/Game/PlayerManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using System.IO;
@@ -12,6 +10,9 @@ namespace OliverLoescher.Multiplayer
         private PhotonView photonView;
         private GameObject playerObject;
 
+        [SerializeField] private GameObject playerPrefab = null;
+        [SerializeField] private GameObject othersPrefab = null; 
+
         private void Awake() 
         {
             photonView = GetComponent<PhotonView>();
@@ -21,13 +22,37 @@ namespace OliverLoescher.Multiplayer
         {
             if (photonView.IsMine)
             {
-                CreateController();
+                SpawnPlayer();
             }
         }
 
-        private void CreateController()
+        public void SpawnPlayer()
         {
-            playerObject = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CharacterController Root"), Vector3.zero, Quaternion.identity);
+            playerObject = Instantiate(playerPrefab);
+            PhotonView pv = playerObject.GetComponent<PhotonView>();
+
+            if (PhotonNetwork.AllocateViewID(pv))
+            {
+                photonView.RPC("RPC_SpawnOtherPlayer", RpcTarget.OthersBuffered, pv.ViewID);
+            }
+            else
+            {
+                Debug.LogError("Failed to allocate a ViewId.");
+                Destroy(playerObject);
+            }
         }
+
+        [PunRPC]
+        public void RPC_SpawnOtherPlayer(int pViewID)
+        {
+            GameObject player = (GameObject) Instantiate(othersPrefab);
+            PhotonView photonView = player.GetComponent<PhotonView>();
+            photonView.ViewID = pViewID;
+        }
+
+        // private void CreateController()
+        // {
+        //     playerObject = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "CharacterController Root"), Vector3.zero, Quaternion.identity);
+        // }
     }
 }
